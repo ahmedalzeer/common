2e49147f0572b2bbebf931aba7b49dc482dc6a19
ahmad ali <ahmad.a@mu.edu.sa>
2020-11-22 09:21:06 +0300

    fix

---
+++ resources/lang/en/app.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'lang'                              => 'Languages List',
+    'url'                               => 'Link',
+    'save'                              => 'Save',
+    'update'                            => 'Update',
+    'edit'                              => 'Edit',
+    'add-category'                      => 'Add Category',
+    'categories'                        => 'Categories',
+
+
+    'ar'   => [
+        'name'                          => 'Arabic Name',
+        'desc'                          => 'Arabic Description',
+    ],
+
+
+    'en'   => [
+        'name'                          => 'Name in English',
+        'desc'                          => 'Description in English',
+    ],
+
+];
